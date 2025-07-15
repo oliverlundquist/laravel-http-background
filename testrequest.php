d72d012ab9ed949bd7b6fb2ea6c846320cc1f5c8
oliverlundquist <mail@oliverlundquist.com>
2025-07-15 14:27:45 +0900

    more windows

--- testrequest.php
+++ testrequest.php
@@ -1,6 +1,16 @@
 <?php
 
+/*
 
+apt-get update -y \
+&& apt-get install -y wget \
+&& wget https://github.com/PowerShell/PowerShell/releases/download/v7.4.11/powershell_7.4.11-1.deb_amd64.deb \
+&& dpkg -i powershell_7.4.11-1.deb_amd64.deb \
+&& apt-get install -f \
+&& rm powershell_7.4.11-1.deb_amd64.deb \
+&& pwsh
+
+*/
 $output = [];
 var_dump(exec('powershell ((curl https://httpbin.org/delay/5 -s &).id)', $output));
 var_dump($output);

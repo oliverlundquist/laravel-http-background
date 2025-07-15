1f0fca21aae640b796cc300173b15b633f15bb9b
oliverlundquist <mail@oliverlundquist.com>
2025-07-15 13:47:32 +0900

    more windows

--- testrequest.php
+++ testrequest.php
@@ -1,11 +1,11 @@
 <?php
 
 
-$id = exec('curl https://httpbin.org/delay/5 -s -o NUL &).id');
+$id = exec('curl https://httpbin.org/delay/5 &).id');
 var_dump($id);
 
 for ($i = 0; $i < 4; $i++) {
-    $status = exec('(Get-Job -Id ' . $id . ').state');
+    $status = exec('Get-Job -Id ' . $id . '');
     var_dump($status);
     sleep(1);
 }

1acd68895616d60d39b80e5517ed251c785bd310
oliverlundquist <mail@oliverlundquist.com>
2025-07-15 13:53:28 +0900

    more windows

--- testrequest.php
+++ testrequest.php
@@ -1,11 +1,15 @@
 <?php
 
 
-$id = exec('curl https://httpbin.org/delay/5 &).id');
-var_dump($id);
+var_dump(exec('which powershell'));
+var_dump(exec('which powershell.exe'));
+var_dump(exec('powershell Get-Job'));
 
-for ($i = 0; $i < 4; $i++) {
-    $status = exec('Get-Job -Id ' . $id . '');
-    var_dump($status);
-    sleep(1);
-}
+// $id = exec('curl https://httpbin.org/delay/5 &).id');
+// var_dump($id);
+
+// for ($i = 0; $i < 4; $i++) {
+//     $status = exec('Get-Job -Id ' . $id . '');
+//     var_dump($status);
+//     sleep(1);
+// }

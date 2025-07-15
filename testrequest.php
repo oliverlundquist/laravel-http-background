b91a45f13b9b0d3555e1f15487fa6af5a9570980
oliverlundquist <mail@oliverlundquist.com>
2025-07-15 14:43:14 +0900

    more windows

--- testrequest.php
+++ testrequest.php
@@ -10,6 +10,18 @@ apt-get update -y \
 && rm powershell_7.4.11-1.deb_amd64.deb \
 && pwsh
 
+shell: C:\Program Files\PowerShell\7\pwsh.EXE -command ". '{0}'"
+  env:
+    OPENSSL_CONF: C:\Program Files\Common Files\SSL\openssl.cnf
+    COMPOSER_PROCESS_TIMEOUT: 0
+    COMPOSER_NO_INTERACTION: 1
+    COMPOSER_NO_AUDIT: 1
+    COMPOSER_AUTH: {"github-oauth": {"github.com": "***"}}
+
+
+$id = exec('pwsh --command "(curl https://httpbin.org/delay/3 -o NUL -s &).id"');
+
+
 */
 $output = [];
 var_dump(exec('powershell ((curl https://httpbin.org/delay/5 -s &).id)', $output));
